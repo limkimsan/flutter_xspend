ad3f620b7db941961892e9c2616fb73f61111284
Kimsanlim <kimsanlim27@gmail.com>
2024-01-03 17:31:13 +0700

    Create static total expense label

--- lib/src/home/home_view.dart
+++ lib/src/home/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'home_header.dart';
 import 'home_transaction_duration.dart';
+import 'package:flutter_xspend/src/constants/colors.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -10,10 +11,36 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
+      // crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        HomeHeader(),
-        HomeTransactionDuration(),
+        const HomeHeader(),
+        const HomeTransactionDuration(),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Container(
+            // decoration: const BoxDecoration(color: Colors.green),
+            margin: const EdgeInsets.only(right: 16, top: 16),
+            child: const Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text('Total expense', style: TextStyle(color: grey)),
+                Text('- KHR 0', style: TextStyle(color: red)),
+                Text('- \$ 0', style: TextStyle(color: red)),
+              ],
+            ),
+          ),
+        )
+
+        // Padding(
+        //   padding: EdgeInsets.all(16),
+        //   child: Column(
+        //     crossAxisAlignment: CrossAxisAlignment.end,
+        //     children: [
+        //       Text('Total expanse', style: TextStyle(color: grey)),
+        //     ],
+        //   ),
+        // ),
       ],
     );
   }

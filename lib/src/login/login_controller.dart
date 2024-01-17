fc49d296eb983e5aef0f9e0baf13bbcd5a3a093f
Kimsanlim <kimsanlim27@gmail.com>
2024-01-17 16:58:36 +0700

    Handle sign up and login offline

--- lib/src/login/login_controller.dart
+++ lib/src/login/login_controller.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:uuid/uuid.dart';
 
 import 'package:flutter_xspend/src/login/login_service.dart';
 import 'package:flutter_xspend/src/utils/api_response_util.dart';
@@ -11,35 +12,50 @@ import 'package:flutter_xspend/src/login/login_view.dart';
 
 class LoginController {
   static Future<void> login(email, password, successCallback, failureCallback) async {
-    if (await InternetConnectionChecker().hasConnection) {
-      try {
-        final response = await LoginService().authenticate(email, password);
-        ApiResponseUtil.handleResponse(response, () async {
-          final SharedPreferences prefs = await SharedPreferences.getInstance();
-          await prefs.setString('TOKEN', jsonDecode(response.body)['access_token']);
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
 
-          // Check if the user is not existed then create new
-          // If the user is already existed then update the serverId of the existing user
-          UserController.handleCreateUpdateUser(email, () {
-            successCallback?.call();
-          });
-        }, (errorMsg) {
-          failureCallback?.call('Failed to login.');
-        });
-      } catch (error) {
-        failureCallback?.call('Failed to login.');
-      }
+    if (await User.isAuthenticationMatched(email, password)) {
+      final user = await User.findByEmail(email);
+      User.markAsLoggedIn(user.id);
+      const uuid = Uuid();
+      await prefs.setString('TOKEN', uuid.v4());
+      successCallback?.call();
     }
     else {
-      if (await User.isAuthenticationMatched(email, password)) {
-        final user = await User.findByEmail(email);
-        User.markAsLoggedIn(user.id);
-        successCallback?.call();
-      }
-      else {
-        failureCallback?.call('User is not existed.');
-      }
+      failureCallback?.call('User is not existed.');
     }
+
+
+    // if (await InternetConnectionChecker().hasConnection) {
+    //   try {
+    //     final response = await LoginService().authenticate(email, password);
+    //     ApiResponseUtil.handleResponse(response, () async {
+    //       await prefs.setString('TOKEN', jsonDecode(response.body)['access_token']);
+
+    //       // Check if the user is not existed then create new
+    //       // If the user is already existed then update the serverId of the existing user
+    //       UserController.handleCreateUpdateUser(email, () {
+    //         successCallback?.call();
+    //       });
+    //     }, (errorMsg) {
+    //       failureCallback?.call('Failed to login.');
+    //     });
+    //   } catch (error) {
+    //     failureCallback?.call('Failed to login.');
+    //   }
+    // }
+    // else {
+    //   if (await User.isAuthenticationMatched(email, password)) {
+    //     final user = await User.findByEmail(email);
+    //     User.markAsLoggedIn(user.id);
+    //     const uuid = Uuid();
+    //     await prefs.setString('TOKEN', uuid.v4());
+    //     successCallback?.call();
+    //   }
+    //   else {
+    //     failureCallback?.call('User is not existed.');
+    //   }
+    // }
   }
 
   static logout(context) async {

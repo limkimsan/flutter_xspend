c71a707b59b53c29f38a435cc728e7aae2dfb5e0
Kimsanlim <kimsanlim27@gmail.com>
2024-01-19 15:07:03 +0700

    Create transaction duration type bottom sheet picker

---
+++ lib/src/home/transaction_duration_bottom_sheet.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_xspend/src/constants/colors.dart';
+import 'package:flutter_xspend/src/shared/bottom_sheet/bottom_sheet_body.dart';
+import 'package:flutter_xspend/src/utils/string_util.dart';
+
+class TransactionDurationBottomSheet extends StatefulWidget {
+  const TransactionDurationBottomSheet({super.key, required this.selectedDuration, required this.updateSelectedDuration});
+
+  final String selectedDuration;
+  final void Function(String duration) updateSelectedDuration;
+
+  void showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) => this,
+    );
+  }
+
+  @override
+  State<TransactionDurationBottomSheet> createState() => _TransactionDurationBottomSheetState();
+}
+
+class _TransactionDurationBottomSheetState extends State<TransactionDurationBottomSheet> {
+  Widget durationPicker() {
+    List durationTypes = ['week', 'month', 'year', 'custom'];
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          for (int i = 0; i < durationTypes.length; i++)
+            Wrap(
+              children: [
+                InkWell(
+                  onTap: () { widget.updateSelectedDuration(durationTypes[i]); },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Flexible(
+                        child: SizedBox(
+                          height: 48,
+                          width: double.infinity,
+                          child: Align(alignment: Alignment.centerLeft, child: Text(StringUtil.capitalize(durationTypes[i])))
+                        ),
+                      ),
+                      if (widget.selectedDuration == durationTypes[i])
+                        const Icon(Icons.check, color: primary)
+                    ],
+                  )
+                ),
+                if (i < durationTypes.length - 1)
+                  const Divider(color: grey, height: 0.2)
+              ]
+            )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      child: BottomSheetBody(
+        title: 'Base currency',
+        titleIcon: const Icon(Icons.currency_exchange_outlined,
+            color: lightGreen, size: 26),
+        body: durationPicker(),
+      ),
+    );
+  }
+}
--- lib/src/home/transaction_duration_picker.dart
+++ lib/src/home/transaction_duration_picker.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_xspend/src/constants/colors.dart';
+import 'transaction_duration_bottom_sheet.dart';
+import 'package:flutter_xspend/src/utils/string_util.dart';
 
 class TransactionDurationPicker extends StatefulWidget {
   const TransactionDurationPicker({super.key});
@@ -10,24 +12,41 @@ class TransactionDurationPicker extends StatefulWidget {
 }
 
 class _TransactionDurationPickerState extends State<TransactionDurationPicker> {
+  String selectedDuration = 'month';
+
+  void onDurationChanged(duration) {
+    setState(() {
+      selectedDuration = duration;
+    });
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: () {
+        TransactionDurationBottomSheet(
+          selectedDuration: selectedDuration,
+          updateSelectedDuration: onDurationChanged
+        ).showBottomSheet(context);
+      },
       child: Container(
         padding: const EdgeInsets.fromLTRB(16, 0, 8, 0),
         alignment: Alignment.center,
         height: 48,
+        width: 115,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
           color: primary,
         ),
-        child: const Row(
+        child: Row(
           children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
-              child: Text('Month', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600)),
+            Expanded(
+              child: Center(
+                child: Text(StringUtil.capitalize(selectedDuration), style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w600)),
+              ),
             ),
-            Icon(Icons.keyboard_arrow_down, color: Colors.white, size: 24),
+            const Icon(Icons.keyboard_arrow_down, color: Colors.white, size: 24),
           ],
         ),
       ),

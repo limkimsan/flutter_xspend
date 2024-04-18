1a4d836c689a2406ee9c767556e80a8b963f96e5
Kimsanlim <kimsanlim27@gmail.com>
2024-04-18 15:29:02 +0700

    Add budget to bottom tab

---
+++ lib/src/budgets/budget_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BudgetView extends StatefulWidget {
+  const BudgetView({super.key});
+
+  @override
+  State<BudgetView> createState() => _BudgetViewState();
+}
+
+class _BudgetViewState extends State<BudgetView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Budget screen');
+  }
+}

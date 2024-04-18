a519810e786578c283d2e7550a3abad1426ac195
Kimsanlim <kimsanlim27@gmail.com>
2024-04-18 16:10:06 +0700

    Create empty budget message widget

--- lib/src/budgets/budget_view.dart
+++ lib/src/budgets/budget_view.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'package:flutter_xspend/src/localization/localization_service.dart';
+import 'package:flutter_xspend/src/constants/colors.dart';
+import 'budget_empty_message.dart';
 
 class BudgetView extends StatefulWidget {
   const BudgetView({super.key});
@@ -10,6 +15,23 @@ class BudgetView extends StatefulWidget {
 class _BudgetViewState extends State<BudgetView> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Budget screen');
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(AppLocalizations.of(context)!.budget)
+      ),
+      body: const BudgetEmptyMessage(),
+      floatingActionButton: SizedBox(
+        height: 56,
+        width: 56,
+        child: FloatingActionButton(
+          foregroundColor: Colors.white,
+          backgroundColor: primary,
+          shape: const CircleBorder(),
+          onPressed: () {},
+          child: const Icon(Icons.add, size: 32)
+        )
+      )
+    );
   }
 }

e6e5882c17e1ab6d6f7e0ee7e49061928468f049
Kimsanlim <kimsanlim27@gmail.com>
2024-04-22 23:12:08 +0700

    Move the wallet detail files into the wallet folder

--- lib/src/routes/app_route.dart
+++ lib/src/routes/app_route.dart
@@ -9,7 +9,7 @@ import 'package:flutter_xspend/src/sign_up/sign_up_view.dart';
 import 'package:flutter_xspend/src/new_transaction/new_transaction_view.dart';
 import 'package:flutter_xspend/src/clean_transaction/clean_transaction_view.dart';
 import 'package:flutter_xspend/src/profile/profile_view.dart';
-import 'package:flutter_xspend/src/wallet_details/wallet_detail_view.dart';
+import 'package:flutter_xspend/src/wallets/wallet_detail_view.dart';
 import 'package:flutter_xspend/src/budgets/new_budget_view.dart';
 import 'package:flutter_xspend/src/isar/isar_service.dart';
 import 'package:flutter_xspend/src/models/user.dart';
--- lib/src/shared/transaction_list/transaction_list.dart
+++ lib/src/shared/transaction_list/transaction_list.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:intl/intl.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 
 import 'package:flutter_xspend/src/constants/colors.dart';

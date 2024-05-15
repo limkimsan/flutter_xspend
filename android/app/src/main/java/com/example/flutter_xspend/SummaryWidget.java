b05d43321209ee203a4f7bf0e2c145755e8a01d3
Kimsanlim <kimsanlim27@gmail.com>
2024-05-16 00:32:17 +0700

    Handle show and hide the vertical and horizontal total section container when the user is resizing the Summary Widget

--- android/app/src/main/java/com/example/flutter_xspend/SummaryWidget.java
+++ android/app/src/main/java/com/example/flutter_xspend/SummaryWidget.java
@@ -4,20 +4,21 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
+import android.widget.LinearLayout;
+import android.view.View;
+import android.os.Bundle;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class SummaryWidget extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
 
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.summary_widget);
         // views.setTextViewText(R.id.appwidget_text, widgetText);
-
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
@@ -30,13 +31,43 @@ public class SummaryWidget extends AppWidgetProvider {
         }
     }
 
-    @Override
-    public void onEnabled(Context context) {
+    // @Override
+    public void onEnabled(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // Enter relevant functionality for when the first widget is created
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.summary_widget);
+        views.setViewVisibility(R.id.summarywidget_total_horizontal_container, View.GONE);
+        for (int appWidgetId : appWidgetIds) {
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
     }
 
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.summary_widget);
+
+        if (newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH) > 272) {
+            views.setViewVisibility(R.id.summarywidget_total_vertical_container, View.GONE);
+            views.setViewVisibility(R.id.summarywidget_total_horizontal_container, View.VISIBLE);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+
+
+            // AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            // appWidgetManager.updateAppWidget(appWidgetId, views);
+            // views.setString(R.id.summarywidget_total_amount, "android:layout_marginTop", "0dp");
+            // views.setString(R.id.summaryWidget_total_amount, "android:gravity", "right");
+        }
+        else {
+            views.setViewVisibility(R.id.summarywidget_total_horizontal_container, View.GONE);
+            views.setViewVisibility(R.id.summarywidget_total_vertical_container, View.VISIBLE);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
     }
 }

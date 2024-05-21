516a4c3fcf36ddbdcfbdfc0f590e00c4a598b423
Kimsanlim <kimsanlim27@gmail.com>
2024-05-21 23:11:20 +0700

    Handle show widget total section in vertical mode on the first time when the summary widget is created

--- android/app/src/main/java/com/example/flutter_xspend/SummaryWidget.java
+++ android/app/src/main/java/com/example/flutter_xspend/SummaryWidget.java
@@ -27,15 +27,6 @@ public class SummaryWidget extends AppWidgetProvider {
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
-    @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.summary_widget);
-        for (int appWidgetId : appWidgetIds) {
-            updateWidgetData(context, appWidgetManager, appWidgetId, views);
-        }
-    }
-
     static void updateWidgetData(Context context, AppWidgetManager appWidgetManager, int appWidgetId, RemoteViews views) {
         SharedPreferences prefs = context.getSharedPreferences("HomeWidgetPreferences", Context.MODE_PRIVATE);
         String income = prefs.getString("income", "0.00");
@@ -48,15 +39,31 @@ public class SummaryWidget extends AppWidgetProvider {
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
-    // @Override
-    public void onEnabled(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // Enter relevant functionality for when the first widget is created
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        SharedPreferences prefs = context.getSharedPreferences("HomeWidgetPreferences", Context.MODE_PRIVATE);
+        boolean isFirstTime = prefs.getBoolean("is_first_time", false);
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.summary_widget);
-        views.setViewVisibility(R.id.summarywidget_total_horizontal_container, View.GONE);
         for (int appWidgetId : appWidgetIds) {
-            appWidgetManager.updateAppWidget(appWidgetId, views);
+            if (isFirstTime) {
+                views.setViewVisibility(R.id.summarywidget_total_amount_horizontal, View.GONE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean("is_first_time", false);
+                editor.apply();
+            }
+            updateWidgetData(context, appWidgetManager, appWidgetId, views);
         }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        SharedPreferences prefs = context.getSharedPreferences("HomeWidgetPreferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("is_first_time", true);
+        editor.apply();
     }
 
     @Override
@@ -70,12 +77,12 @@ public class SummaryWidget extends AppWidgetProvider {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.summary_widget);
 
         if (newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH) > 272) {
-            views.setViewVisibility(R.id.summarywidget_total_vertical_container, View.GONE);
-            views.setViewVisibility(R.id.summarywidget_total_horizontal_container, View.VISIBLE);
+            views.setViewVisibility(R.id.summarywidget_total_amount_vertical, View.GONE);
+            views.setViewVisibility(R.id.summarywidget_total_amount_horizontal, View.VISIBLE);
         }
         else {
-            views.setViewVisibility(R.id.summarywidget_total_horizontal_container, View.GONE);
-            views.setViewVisibility(R.id.summarywidget_total_vertical_container, View.VISIBLE);
+            views.setViewVisibility(R.id.summarywidget_total_amount_horizontal, View.GONE);
+            views.setViewVisibility(R.id.summarywidget_total_amount_vertical, View.VISIBLE);
         }
         updateWidgetData(context, appWidgetManager, appWidgetId, views);
     }
